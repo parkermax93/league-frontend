286e635ae394c71dd8157529ef7fe7bc84ac816e
parkermax93 <parker.williamson@gmail.com>
2020-08-27 08:54:20 -0700

    clean up unneeded code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-document.addEventListener('DOMContentLoaded', ()=> alert('hi'))
+const LEAGUE_URL = "http://localhost:3000/"
+
+class Character {
+    constructor(name, imageLink, )
+    this.name = name;
+    this.imageLink= recipeLink;
+
+}
