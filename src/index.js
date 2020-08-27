45c32dc3b94cd9ce5331a8301b530698ab641914
parkermax93 <parker.williamson@gmail.com>
2020-08-27 10:57:40 -0700

    fixing card

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const CHARACTERS_URL = "http://localhost:3000/characters"
+const LANES_URL = "http://localhost:3000/lanes"
+const formSubmit = document.getElementById("form-submit")
+const formButtons = document.getElementById("form-show-buttons")
+const addCharacterButton = document.getElementById("add-character")
+const dropDownButton = document.getElementById("filter-button")
+const laneDropDown = document.getElementById("filter-dropdown")
+const cardContainer = document.getElementById('character-card-container')
 
 class Character {
-    constructor(name, imageLink, laneID)
-    this.name = name;
-    this.imageLink= imageLink;
-    this.laneID= laneID;
-}
+    constructor(name, imageLink, laneId) {
+    this.name = name; 
+    this.imageLink = imageLink;
+    this.laneId = laneId;
+    }
 
-class createCharacterCard() {
-        const cardContainer = document.getElementById('character-card-container')
+    createCharacterCard() {
         const card = document.CreateElement('div')
         card.className = "card"
         const img = document.createElement('img')
@@ -20,11 +26,15 @@ class createCharacterCard() {
         name.innerHTML = this.name 
         cardInfo.appendChild(name)
         const ul = document.createElement('ul')
-        for (lane of this.laneID) {
+        for (lane of this.laneId) {
             let li = document.createElement('li')
             li.innerHTML = lane 
             ul.appendChild(li)
         }
+        cardInfo.appendChild(ul)
+        card.appendChild(cardInfo)
+        cardContainer.appendChild(card)
+    }
 }
 
 function getCharacters() {
@@ -67,4 +77,75 @@ document.addEventListener("DOMContentLoaded", function() {
     laneDropDown.addEventListener("change", function() {
         getRandomCharacterByLane();
     })
+})
+
+function toggleForm() {
+    const form = formSubmit.parentElement;
+    if (form.classList.contains("hidden")) {
+        form.classList.remove("hidden");
+    } else {
+        form.className += " hidden";
+    }
+}
+
+function clearCharacters() {
+    cardContainer.innerHTML = ""
+}
+
+function toggleDropDown() {
+    const dropDown = document.getElementById("filter-drop-down")
+    if (dropDown.classList.contains("hidden")) {
+        drownDown.classList.remove("hidden");
+    } else {
+        dropDown.ClassName += " hidden"
+    }
+    getLanes();
+}
+
+function getLanes() {
+    fetch(LANES_URL).then(response => response.json()).then(json => populateLaneDropDown(json.data))
+}
+
+function populateLaneDropDown(data) {
+    console.log(data)
+    for (lane of data) {
+        let option = document.createElement("option")
+        option.value = lane.attributes.name
+        option.innerHTML = lane.attributes.name
+        laneDropDown.appendChild(option)
+    }
+}
+
+function toggleButtons() {
+    if (formButtons.classList.contains("hidden")) {
+        formButtons.classList.remove("hidden");
+    } else {
+        formButtons.className += " hidden";
+    }
+}
+
+function addCharacter() {
+    const form = event.target.parentElement 
+    const lane = form[3].value.split(', ')
+    const character = new Character(form[0].value, form[1].value, form[2].value)
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": form[0].value,
+            "image_link": form[1].value,
+            "lane_id": form[2].value,
+        })
+    };
+    fetch(CHARACTERS_URL, configurationObject)
+    .then(response => response.json())
+    .then(function(json) {
+        character.createCharacterCard();
+        toggleButtons();
+        toggleForm();
+    })
+    .catch(error => console.log("Error: " + error))
 }
