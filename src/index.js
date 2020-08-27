2f1d02aaf12f0ee5676204d75c27b9b26f19fc41
parkermax93 <parker.williamson@gmail.com>
2020-08-27 08:56:34 -0700

    update index js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const LEAGUE_URL = "http://localhost:3000/"
 class Character {
     constructor(name, imageLink, )
     this.name = name;
-    this.imageLink= recipeLink;
+    this.imageLink= imageLink;
 
 }
